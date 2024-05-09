93bd204e9b18bb09a36bbb6be080f8dc99de95d5
miracleexotic <mai_nutthawat@hotmail.com>
2024-05-09 22:33:47 +0700

    [ADD] EIP - Add toggle service On/Off.

--- eip-workspace/GSLB/webportal/frontend/src/Components/Pages/Welcome.tsx
+++ eip-workspace/GSLB/webportal/frontend/src/Components/Pages/Welcome.tsx
@@ -1,8 +1,114 @@
+import * as React from "react";
 import { Typography } from "@mui/material";
 import Box from "@mui/material/Box";
 import Container from "@mui/material/Container";
+import FormGroup from "@mui/material/FormGroup";
+import Stack from "@mui/material/Stack";
+import Switch, { SwitchProps } from "@mui/material/Switch";
+import { styled } from "@mui/material/styles";
+
+const AntSwitch = styled(Switch)(({ theme }) => ({
+  width: 28,
+  height: 16,
+  padding: 0,
+  display: "flex",
+  "&:active": {
+    "& .MuiSwitch-thumb": {
+      width: 15,
+    },
+    "& .MuiSwitch-switchBase.Mui-checked": {
+      transform: "translateX(9px)",
+    },
+  },
+  "& .MuiSwitch-switchBase": {
+    padding: 2,
+    "&.Mui-checked": {
+      transform: "translateX(12px)",
+      color: "#fff",
+      "& + .MuiSwitch-track": {
+        opacity: 1,
+        backgroundColor: theme.palette.mode === "dark" ? "#177ddc" : "#1890ff",
+      },
+    },
+  },
+  "& .MuiSwitch-thumb": {
+    boxShadow: "0 2px 4px 0 rgb(0 35 11 / 20%)",
+    width: 12,
+    height: 12,
+    borderRadius: 6,
+    transition: theme.transitions.create(["width"], {
+      duration: 200,
+    }),
+  },
+  "& .MuiSwitch-track": {
+    borderRadius: 16 / 2,
+    opacity: 1,
+    backgroundColor:
+      theme.palette.mode === "dark"
+        ? "rgba(255,255,255,.35)"
+        : "rgba(0,0,0,.25)",
+    boxSizing: "border-box",
+  },
+}));
 
 function WelcomePage() {
+  const [checked, setChecked] = React.useState(true);
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setChecked(event.target.checked);
+
+    let data = {
+      state: event.target.checked,
+    };
+
+    console.log(data);
+
+    const apiUrl = `${process.env.REACT_APP_BACKEND_API}/service`;
+    const requestOptions = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    };
+
+    fetch(apiUrl, requestOptions)
+      .then((response) => response.json())
+      .then((res) => {
+        console.log(res);
+        if (res) {
+          console.log("SUCCESSED");
+        } else {
+          console.log("FAILED");
+        }
+      });
+  };
+
+  const getServiceState = () => {
+    const apiUrl = `${process.env.REACT_APP_BACKEND_API}/healthcheck`;
+    const requestOptions = {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+
+    fetch(apiUrl, requestOptions)
+      .then((response) => response.json())
+      .then((res) => {
+        console.log(res);
+        if (res.data) {
+          setChecked(res.data.state);
+        } else {
+          // setError(true);
+        }
+      });
+  };
+
+  React.useEffect(() => {
+    getServiceState();
+  }, []);
+
   return (
     <div
       style={{
@@ -28,6 +134,19 @@ function WelcomePage() {
           >
             <b>WELCOME</b>
           </Typography>
+          <Box sx={{ display: "flex", justifyContent: "center", marginTop: 1 }}>
+            <FormGroup>
+              <Stack direction="row" spacing={1} alignItems="center">
+                <Typography>Off</Typography>
+                <AntSwitch
+                  checked={checked}
+                  onChange={handleChange}
+                  inputProps={{ "aria-label": "ant design" }}
+                />
+                <Typography>On</Typography>
+              </Stack>
+            </FormGroup>
+          </Box>
         </Box>
       </Container>
     </div>
